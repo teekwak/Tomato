499a4c43e0561c393aa193d30d93257e079e3fc1
Thomas Kwak <teekwak@gmail.com>
2017-08-15 12:03:37 -0700

    Added notification example and eslint file

---
+++ renderer.js
@@ -0,0 +1,9 @@
+function doSomething() {
+	let myNotification = new Notification('Title', {
+	  body: 'Lorem Ipsum Dolor Sit Amet'
+	})
+
+	myNotification.onclick = () => {
+	  console.log('Notification clicked')
+	}
+}
