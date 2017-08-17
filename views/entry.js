5e51d39b5fd6db13c2dd7b94c0b018b91f6766d9
Thomas Kwak <teekwak@gmail.com>
2017-08-16 20:32:21 -0700

    Removed commented out code from entry point

--- views/entry.js
+++ views/entry.js
@@ -1,13 +1,3 @@
-// function doSomething() {
-// 	let myNotification = new Notification('Title', {
-// 	  body: 'Lorem Ipsum Dolor Sit Amet'
-// 	})
-
-// 	myNotification.onclick = () => {
-// 	  console.log('Notification clicked')
-// 	}
-// }
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../src/App'
