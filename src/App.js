200b5175e9e44c87060c21e36c40084f1fa35520
Thomas Kwak <teekwak@gmail.com>
2017-08-16 21:41:16 -0700

    Added date container for showing date and time

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import DateContainer from './DateContainer';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <DateContainer />
+
         <button onClick={() => {
           let myNotification = new Notification('Notification Title', {
             body: 'This is the notification body'
---
+++ src/DateContainer.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+class DateContainer extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      date: '',
+      time: ''
+    };
+    this.interval = null;
+    this.months = [
+      'January', 
+      'February', 
+      'March', 
+      'April', 
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+  }
+
+  computeCurrentDateAndTime() {
+    const dateObject = new Date();
+    const month = this.months[dateObject.getMonth()];
+    const day = dateObject.getDay();
+    const year = dateObject.getFullYear();
+
+    let hour = dateObject.getHours();
+    let minute = dateObject.getMinutes();
+    let second = dateObject.getSeconds();
+    let ampm = 'am';
+
+    if(hour == 0) {
+      hour = 12;
+    } else if (hour > 12) {
+      hour -= 12;
+      ampm = 'pm';
+    }
+
+    if(minute < 10) {
+      minute = '0' + minute;
+    }
+
+    if(second < 10) {
+      second = '0' + second;
+    }
+
+    this.setState({
+      date: month + ' ' + day + ' ' + year,
+      time: hour + ':' + minute + ':' + second + ' ' + ampm
+    });
+  }
+
+  componentDidMount() {
+    this.computeCurrentDateAndTime(); // render immediately upon mounting
+    this.interval = setInterval(() => this.computeCurrentDateAndTime(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.date}</h1>
+        <h3>{this.state.time}</h3>
+      </div>
+    )
+  }
+}
+
+export default DateContainer;
